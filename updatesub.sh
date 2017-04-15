128d0f264fbdfd7c476154b946e4dd6d86d54fbc
dsirius <dmodoomsirius@users.noreply.github.com>
2017-04-15 00:24:56 -0400

    add sh version to update sub modules

---
+++ updatesub.sh
@@ -0,0 +1,15 @@
+cd API
+git checkout master
+cd ../CodeGenerator
+git checkout master
+cd ../Engine
+git checkout development
+cd ../Lib-Access
+git checkout master
+cd ../ModCompat
+git checkout master
+cd ../ModFlags
+git checkout master
+cd ../Prefabs
+git checkout master
+git submodule foreach git pull
