0a160e5b069042a893dc71ac27f7a26a3f01a420
dsirius <dmodoomsirius@users.noreply.github.com>
2017-04-19 01:27:10 -0400

    update CodingLib Version

--- src/templates/java/com/builtbroken/mc/codegen/templates/TileTemplateExternalInventory.java
+++ src/templates/java/com/builtbroken/mc/codegen/templates/TileTemplateExternalInventory.java
@@ -2,8 +2,8 @@ package com.builtbroken.mc.codegen.templates;
 
 import com.builtbroken.mc.api.tile.provider.IInventoryProvider;
 import com.builtbroken.mc.codegen.template.TileWrappedTemplate;
-import com.builtbroken.mc.framework.logic.ITileNode;
-import com.builtbroken.mc.framework.logic.annotations.ExternalInventoryWrapped;
+import com.builtbroken.mc.api.tile.node.ITileNode;
+import com.builtbroken.mc.codegen.annotations.ExternalInventoryWrapped;
 import com.builtbroken.mc.framework.logic.wrapper.TileEntityWrapper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
