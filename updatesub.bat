4221995033085cf30c4b7eac3f99efbfe3ccc70c
dsirius <dmodoomsirius@users.noreply.github.com>
2018-01-28 01:31:01 -0500

    initial 1.12.2 commit

--- updatesub.bat
+++ updatesub.bat
@@ -1,18 +1,18 @@
 @echo off
 cd API
-git pull origin master
+git pull origin 1.12
 cd ../CodeGenerator
 git pull origin master
 cd ../Engine
-git pull origin development
+git pull origin 1.12
 cd ../Lib-Access
-git pull origin master
+git pull origin 1.12
 cd ../ModCompat
 git pull origin master
 cd ../ModFlags
 git pull origin master
 cd ../Prefabs
-git pull origin master
+git pull origin 1.12
 cd ../Minecraft-1.7
 git pull origin master
 cd ..
